4530ab988b65a37b691b99f4796c404064208c40
flamma <lukebarbier12@gmail.com>
2019-04-30 18:10:09 +0000

    jump configs

--- magic_sm/src/magic_states/runway.py
+++ magic_sm/src/magic_states/runway.py
@@ -14,6 +14,9 @@ class Runway(Magic_State):
         self.yaw = 0 # Determine if PID is needed
         rospy.Subscriber('/imu/data/', Imu, self.imu_callback)
         self.imu_thresh = float(rospy.get_param('imu_thresh'))
+        self.accel_z = None
+        num_terms_to_avg = 50
+        self.beta = (1.0 - float(num_terms_to_avg)) / (- float(num_terms_to_avg))
 
     def calculate_drive(self, distance, jump_angle):
         # TODO actually calculate the required drive
@@ -26,6 +29,11 @@ class Runway(Magic_State):
             rate.sleep()
         return "airborne"
 
-    def imu_callback(self, msg): # May need to do some filtering on this...
+    def imu_callback(self, msg):
+        if self.accel_z == None:
+            self.accel_z = msg.linear_acceleration.z
+        else:
+            self.accel_z = (self.beta * self.accel_z) + (1-self.beta) * msg.linear_acceleration.z
+            
         if abs(msg.linear_acceleration.z) < self.imu_thresh:
             self.airborne = True
