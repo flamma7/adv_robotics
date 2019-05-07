245446ca94b025820791ca5c11104973259a4b43
Brandon Aguirre <brag4918@colorado.edu>
2019-05-07 13:25:15 -0600

    small adjustment to class

--- measurements/DataCube.py
+++ measurements/DataCube.py
@@ -1,11 +1,14 @@
 import numpy as np
+import pickle
+from os.path import join, isfile
+import sys
 
 class DataCube:
   def __init__(self, shape):
     if len(shape) != 3:
       raise Exception('shape should be of length 3, not {}'.format(len(shape)))
     self.data = np.zeros(
-      shape=tuple(int((s[1]['max']-s[1]['min']) / s[1]['inc']) for s in shape)
+      shape=tuple(int(np.ceil((s[1]['max']-s[1]['min']) / s[1]['inc'])) for s in shape)
     )
     
     self.shape = { i:{'name': shape[i][0][0],
@@ -17,9 +20,7 @@ class DataCube:
     
     self.imap = { shape[i][0][0]: i for i in range(len(shape))}
   
-  def sets(self, setpoints, value):
-    if len(setpoints) != 3:
-      raise Exception('shape should be of length 3, not {}'.format(len(shape)))
+  def dmap(self, setpoints):
     o = [0]*3
     for setpoint in setpoints:
       col = self.imap[setpoint[0]]
@@ -27,18 +28,26 @@ class DataCube:
         raise Exception('setpoint "{}" with value {} is out of range [{}, {}]'
           .format(setpoint[0], setpoint[1], self.shape[col]['min'], self.shape[col]['max'])
         )
-      o[col] = int((setpoint[1]-self.shape[col]['min']) / self.shape[col]['inc'])
+      o[col] = int((setpoint[1]- self.shape[col]['min']) / self.shape[col]['inc'])
+    return o
+
+  def sets(self, setpoints, value):
+    if len(setpoints) != 3:
+      raise Exception('shape should be of length 3, not {}'.format(len(shape)))
+    o = self.dmap(setpoints)
     self.data[o[0]][o[1]][o[2]] = value
   
   def gets(self, setpoints):
     if len(setpoints) != 3:
       raise Exception('shape should be of length 3, not {}'.format(len(shape)))
-    o = [0]*3
-    for setpoint in setpoints:
-      col = self.imap[setpoint[0]]
-      if setpoint[1] < self.shape[col]['min'] or setpoint[1] > self.shape[col]['max']:
-        raise Exception('setpoint "{}" with value {} is out of range [{}, {}]'
-          .format(setpoint[0], setpoint[1], self.shape[col]['min'], self.shape[col]['max'])
-        )
-      o[col] = int((setpoint[1]-self.shape[col]['min']) / self.shape[col]['inc'])
+    o = self.dmap(setpoints)
     return self.data[o[0]][o[1]][o[2]]
+
+  def get_slice(self):
+    pass
+    
+  def read_csv():
+    pass
+
+  def put_csv():
+    pass
