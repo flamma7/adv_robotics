7c5ce3ffb14a1b8aad9f1ae2703f0760d242efc3
sousheel <sousheel.vunnam@colorado.edu>
2019-05-06 19:33:41 -0600

    magic_jump node

--- magic_vel/scripts/magic_jump.py
+++ magic_vel/scripts/magic_jump.py
@@ -21,18 +21,33 @@ class MagicJump():
 
 		# Retrieve IR distance
 		rospy.Subscriber("/front_distance", std_msgs.Float64, self.getFrontDistance)
-		self.front_dist = 0
+		self.cur_dist = 0
+        self.prev_dist = 0
         self.vel_pub = rospy.Publisher("/velocity", std_msgs.Float64)
 
-        self.freq = 1/50
-        self.timer = rospy.Timer(rospy.Duration(self.freq), self.freq_callback)
+        self.ir_freq = 1/50
+        self.timer = rospy.Timer(rospy.Duration(self.ir_freq), self.ir_freq_callback)
+
+        self.vel_freq = 1/10
+        self.vel_pub_timer = rospy.Timer(rospy.Duration(self.vel_freq), self.vel_callback)
+
+        self.vels = []
 
 
     def getFrontDistance(self, data):
-        self.front_dist = data
+        self.prev_dist = self.cur_dist
+        self.cur_dist = data
 
-    def freq_callback(self):
+    def ir_freq_callback(self):
         pass
 
+    def vel_callback(self):
+        if len(self.vels) < 5:
+            return
+        self.vel_pub.publish(np.mean(self.vels[-5:]))
+
 if __name__=="__main__":
-    main()
+    try:
+		main()
+	except rospy.ROSInterruptException:
+		pass
